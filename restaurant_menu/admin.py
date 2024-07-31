8ccc0654fd2994ebb9f653257def4c876d174b9f
CSL <christasaraluke19@gmail.com>
2024-07-31 13:51:24 -0700

    Creating admin interface and added data in it.

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status")
+    list_filter = ("status", )
+    search_fields = ("meal", )
+
+
+admin.site.register(Item, MenuAdmin)
+
--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -9,8 +9,8 @@ MEAL_TYPE = (
 )
 
 STATUS = (
-    (0, "Unavailable"),
-    (1, "Available"),
+    ("0", "Unavailable"),
+    ("1", "Available"),
 )
 
 
@@ -18,8 +18,8 @@ class Item(models.Model):
     meal = models.CharField(max_length=1000)
     meal_description = models.CharField(max_length=2000)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    meal_type = models.CharField(max_length=200, choices=MEAL_TYPE)
-    status = models.CharField(max_length=200, choices=STATUS)
+    meal_type = models.CharField(max_length=200, choices=MEAL_TYPE, default="main_dishes")
+    status = models.CharField(max_length=200, choices=STATUS, default="1")
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
