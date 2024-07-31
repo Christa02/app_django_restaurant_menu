c5900134d7fc9e4c50b979fc84ca24b90398b1f6
CSL <christasaraluke19@gmail.com>
2024-07-31 13:26:58 -0700

    Context in django.

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -7,6 +7,11 @@ class MenuListView(generic.ListView):
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {"meals": ["Pizza", "Pancake"],
+                   "ingredients": ["dough"]}
+        return context
+
 
 class MealDetailView(generic.DetailView):
     model = Item
